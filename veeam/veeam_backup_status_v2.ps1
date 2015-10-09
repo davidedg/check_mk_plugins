1ddd4d84fa54ba97b0428561615b755706c031f2
Davide Del Grande <delgrande.davide@gmail.com>
2015-10-09 11:51:26 +0200

    Version 1.1: Enhancements to PS console, taken from original plugin in Check_MK 1.2.6p12 Fixed broken output on some non-english systems (time separator) Spaces in VM names are now replaced with underscores, to behave better together with VMWare checks

--- veeam/veeam_backup_status_v2.ps1
+++ veeam/veeam_backup_status_v2.ps1
@@ -6,13 +6,18 @@
 # directory given in veeam_backup_status.bat and the .bat file needs to be
 # started by the check_mk agent instead.
 
-# v1.0
+# v1.0 - initial release:
 # Author: Davide Del Grande <davide.delgrande _ lanewan.it> / <delgrande.davide _ gmail.com>
 # Based on original veeam_backup_status.ps1 in Check_MK 1.2.6p2
 # Enhancements over the original plugin:
 # - Supports "retried" jobs by searching all sessions started within last $HoursToCheck hours
 # - No Duplicates: a VM is reported only ONCE, even if it's in multiple Jobs (most recent state)
 # - VMs are reported based on session Creationtime rather than EndTime (most recent snapshot creation = more recent data)
+# v1.1 - fixes:
+# Enhancements to PS console, taken from original plugin in Check_MK 1.2.6p12
+# Fixed broken output on some non-english systems (time separator)
+# Spaces in VM names are now replaced with underscores, to behave better together with VMWare checks
+
 
 
 # The script will check ALL Sessions that have been *STARTED* by ENABLED backups within last $HoursToCheck:
@@ -22,6 +27,16 @@ $HoursToCheck = 168 # change this at your needs.
 # If True, it will include Jobs' LAST SESSION, even if it falls outside $HoursToCheck search window
 # This mimics the original plugin behaviour
 $AlwaysIncludeLastSession = $True
+
+
+# Configure PS console
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.buffersize
+$newsize.height = 300
+$newsize.width = 150
+$pswindow.buffersize = $newsize
+
 
 # Load Veeam Backup and Replication Powershell Snapin
 $snapin="VeeamPSSnapIn"
@@ -86,21 +101,21 @@ $ScheduledJobs | ForEach-Object {
 	$myJobLastState = $_.GetLastState()
 	$myJobLastResult = $_.GetLastResult()
 	$myJobLastSession = $_.FindLastSession()
-	$myJobCreationTime = $myJobLastSession.CreationTime |  get-date -Format "dd.MM.yyyy HH:mm:ss"  -ErrorAction SilentlyContinue
-	$myJobEndTime = $myJobLastSession.EndTime |  get-date -Format "dd.MM.yyyy HH:mm:ss"  -ErrorAction SilentlyContinue
-	$myJobsText = $myJobsText + $_.Name + "`t" + $_.JobType + "`t" + $myJobLastState + "`t" + $myJobLastResult + "`t" + $myJobCreationTime + "`t" + $myJobEndTime + "`n"
+	$myJobCreationTime = $myJobLastSession.CreationTime |  get-date -Format "dd.MM.yyyy HH\:mm\:ss"  -ErrorAction SilentlyContinue
+	$myJobEndTime = $myJobLastSession.EndTime |  get-date -Format "dd.MM.yyyy HH\:mm\:ss"  -ErrorAction SilentlyContinue
+	$myJobsText = $myJobsText + ($_.Name -replace "\'","_" -replace " ","_") + "`t" + $_.JobType + "`t" + $myJobLastState + "`t" + $myJobLastResult + "`t" + $myJobCreationTime + "`t" + $myJobEndTime + "`n"
 }
 
 # VM Section
 $myTaskText = "" # Create new text string for backup tasks section.
 # $vms.GetEnumerator() | Sort-Object Name | ForEach-Object { ## For DEBUG purposes, you may want to sort output by VM (hash key name)
 $vms.GetEnumerator() | ForEach-Object {
-	$myTaskStartTime = $_.value.Progress.StartTime |  get-date -Format "dd.MM.yyyy HH:mm:ss"  -ErrorAction SilentlyContinue
-	$myTaskStopTime = $_.value.Progress.StopTime |  get-date -Format "dd.MM.yyyy HH:mm:ss"  -ErrorAction SilentlyContinue
+	$myTaskStartTime = $_.value.Progress.StartTime |  get-date -Format "dd.MM.yyyy HH\:mm\:ss"  -ErrorAction SilentlyContinue
+	$myTaskStopTime = $_.value.Progress.StopTime |  get-date -Format "dd.MM.yyyy HH\:mm\:ss"  -ErrorAction SilentlyContinue
 	# Result is a value of type System.TimeStamp. I'm sure there is a more elegant way of formatting the output:
 	$myTaskDuration = "{0:D2}" -f $_.value.Progress.duration.Days + ":" + "{0:D2}" -f $_.value.Progress.duration.Hours + ":" + "{0:D2}" -f $_.value.Progress.duration.Minutes + ":" + "{0:D2}" -f $_.value.Progress.duration.Seconds
 
-	$myTaskText = $myTaskText + "<<<<" + $_.value.Name + ">>>>" + "`n"
+	$myTaskText = $myTaskText + "<<<<" + ($_.value.Name -replace " ","_") + ">>>>" + "`n"
 	$myTaskText = $myTaskText + "<<<"+ "veeam_client:sep(9)" +">>>" +"`n"
 	$myTaskText = $myTaskText + "Status" + "`t" + $_.value.Status + "`n"
 	$myTaskText = $myTaskText + "JobName" + "`t" + $_.value.JobSess.OrigJobName + "`n"
